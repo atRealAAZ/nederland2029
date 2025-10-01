b3f01f593e3ea74596dfd0f0d69f514cb50431ed
Arthur Zwartsenberg <a.a.zwartsenberg@gmail.com>
2025-10-01 17:10:03 +0200

    added allowed nederland25 subdomains

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:5173", 
+        "https://nederland2025.app",
+        "https://www.nederland2025.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
